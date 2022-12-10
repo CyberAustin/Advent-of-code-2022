7d52f2fdbdba2b7d21846f3407610d2fab8c54e5
CyberAustin <austin.y.schibler@gmail.com>
2022-12-10 11:00:46 -0600

    day 6 solved

--- day6/day6.go
+++ day6/day6.go
@@ -37,22 +37,25 @@ func part1(input string) {
 
 }
 
-func part2(input string) {
-	//14 instead of 4. Fuck me.
-	for i, v := range input {
-		if i < 3 {
-			continue
+func isMarker(arr string) bool {
+	m := make(map[rune]bool)
+	for _, i := range arr {
+		_, ok := m[i]
+		if ok {
+			return false
 		}
-		if input[i] != input[i-1] && input[i] != input[i-2] && input[i] != input[i-3] {
-			if input[i-1] != input[i-2] && input[i-1] != input[i-3] {
-				if input[i-2] != input[i-3] {
-					fmt.Printf("%d:  %s\n", i+1, string(v))
-					break
-				}
-			}
+		m[i] = true
+	}
+	return true
+}
+
+func part2(input string) {
+	for i := 0; i < len(input)-14; i++ {
+		if isMarker(input[i : i+14]) {
+			fmt.Printf("%d:  %s\n", i+14, string(input[i+14]))
+			break
 		}
 	}
-
 }
 
 func main() {
